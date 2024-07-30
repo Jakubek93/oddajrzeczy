bd7fbd0df5f7aff9280bf4df2019081b0dcaab10
Jakubek93 <jnjakubnowak1993@gmail.com>
2024-07-30 17:22:49 +0200

    test

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,3 @@
-/*
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
@@ -9,14 +8,15 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-*/
 
+/*
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY,
 );
+*/
 
 /*
 /!*import { createClient } from '@supabase/supabase-js'
